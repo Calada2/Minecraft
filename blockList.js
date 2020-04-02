a786bc87178392b52efe460c0164a05f567ec8d2
Oliver Sepsik <39313944+Calada2@users.noreply.github.com>
2020-04-02 23:57:34 +0200

    Add files via upload

---
+++ blockList.js
@@ -0,0 +1,68 @@
+const blockList = [
+    {
+        name: 'Air'
+    },
+    {
+        pic: 'dirt.png',
+        name: 'Dirt',
+        multiside: false
+    },
+    {
+        pic: 'grass_block_top.png',
+        name: 'Grass',
+        multiside: true,
+        sides: {
+            0: 'grass_block_side.png',
+            1: 'grass_block_side.png',
+            2: 'grass_block_side.png',
+            3: 'grass_block_side.png',
+            4: 'grass_block_top.png',
+            5: 'dirt.png',
+        }
+    },
+    {
+        pic: 'stone.png',
+        name: 'Stone',
+        multiside: false
+    },
+    {
+        pic: 'cobblestone.png',
+        name: 'Cobblestone',
+        multiside: false
+    },
+    {
+        pic: 'sand.png',
+        name: 'Sand',
+        multiside: false
+    },
+    {
+        pic: 'bedrock.png',
+        name: 'Bedrock',
+        multiside: false
+    },
+    {
+        pic: 'oak_planks.png',
+        name: 'Planks',
+        multiside: false
+    },
+    {
+        pic: 'cobblestone_red.png',
+        name: 'Red Cobblestone',
+        multiside: false
+    },
+    {
+        pic: 'cobblestone_blue.png',
+        name: 'Blue Cobblestone',
+        multiside: false
+    },
+    {
+        pic: 'cobblestone_green.png',
+        name: 'Green Cobblestone',
+        multiside: false
+    },
+    {
+        pic: 'cobblestone_yellow.png',
+        name: 'Yellow Cobblestone',
+        multiside: false
+    },
+];
